c47c672861cb0a9b0b78e77d9da3b490f562506a
George Hanna <georgehanna.sk@gmail.com>
2017-11-20 12:43:39 -0500

    Epoched Data according to four classes

--- basicLDA.py
+++ basicLDA.py
@@ -8,10 +8,11 @@ import numpy as np
 import EOGRemove_LR
 import matplotlib.pyplot as plt
 
+'''Load Data'''
+'''-------------------------------------------'''
 #Data path
 datapath = '/Users/georgehanna/Projects/MotorImagery/Data/A01T.gdf'
 data_obj = mne.io.read_raw_edf(datapath, preload=True, stim_channel=None)
-
 #Visualize Data channels
 #data_obj.plot()
 
@@ -21,32 +22,47 @@ event_duration = data_obj._raw_extras[0]['events'][4]
 event_latency = data_obj._raw_extras[0]['events'][1]
 stim_data = np.zeros((1, len(data_obj.times)))
 
-
-#TO-Do I just store onset times or do I incorporate duration? For now just doing onset
-#TODO handle duplicate events at same latency
+'''Here the event code is stored at the latency time specified by event_latency
+Note that if a trial is rejected, (code 1023) will appear at the same latency as as the original event code (768)
+'''
 for i in range(len(event_type)):
-    stim_data[0,event_latency[i]] = event_type[i]
+    #if the trial is rejected, don't store the event code
+    if (i!=0 and event_type[i-1] != 1023):
+        stim_data[0,event_latency[i]] = event_type[i]
+
 
 info = mne.create_info(['STI'],data_obj.info['sfreq'],['stim'])
 stim_raw = mne.io.RawArray(stim_data, info)
 data_obj.add_channels([stim_raw],force_update_info =True)
 
-print(data_obj)
+#print(data_obj)
 
-#Visualize events (for debugging)
-events = mne.find_events(data_obj,stim_channel='STI',verbose=True)
-event_id = {"left": 769,"right":770, "foot":771,"tongue": 772, "EyeOpen": 276, "EyeClosed": 277, "StartTrial": 768, "Unknown"
-: 783, "EyeMove": 1072, "Rejected":1023, "StartRun":32766 }
-
-#mne.viz.plot_events(events,data_obj.info['sfreq'],data_obj.first_samp, event_id =event_id, show=True)
-#print(events)
-
-#EOG Removal
+'''EOG Removal'''
+'''--------------------------------------'''
+#Extract EEG only channels
 Noisy_EEG = np.transpose(data_obj.get_data()[0:22,:])
+#Extract EOG only channels
 EOG = np.transpose(data_obj.get_data()[22:25,:])
+#Return cleaned up EEG and store it back into data object
 cleanEEG = EOGRemove_LR.EOGRemove(Noisy_EEG,EOG)
 data_obj._data[0:22] = cleanEEG.T
 
-data_obj.plot()
+#data_obj.plot()
+
+'''Epoch Dataset'''
+'''--------------------------------------'''
+#Visualize events (for debugging)
+events = mne.find_events(data_obj,stim_channel='STI',verbose=True)
+event_id = {"left": 769,"right":770, "foot":771,"tongue": 772 }
+#mne.viz.plot_events(events,data_obj.info['sfreq'],data_obj.first_samp, event_id =event_id, show=True)
+
+#Define time constraints on epochs (based on data collection paradigm defined in dataset PDF)
+tmin, tmax = 0., 4.
+#Pick EEG channels from dataset
+picks = np.arange(0,22)
+
+# Read epochs
+epochs = mne.Epochs(data_obj, events, event_id, tmin, tmax, proj=True, picks=picks,
+                baseline=None, preload=True)
 
 print("done")
